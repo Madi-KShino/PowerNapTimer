69289e988a6880d572b43fbcb0889ee76718f2ae
Madison Kaori Shino <kaorishino@Madisons-MacBook-Pro.local>
2019-06-18 10:52:17 -0600

    Basic File Set up

---
+++ PowerNaps/View Controllers/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  PowerNaps
+//
+//  Created by Madison Kaori Shino on 6/18/19.
+//  Copyright © 2019 Madi S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var napButton: UIButton!
+    
+    let timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    func updateLabel() {
+        
+    }
+    
+    func updateButton() {
+        
+    }
+    
+    func startTimer() {
+        
+    }
+    
+    func stopTimer() {
+        
+    }
+    
+    func setTimer() {
+        
+    }
+}
+
